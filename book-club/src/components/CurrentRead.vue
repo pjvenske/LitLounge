45c45370a9bd35ca8fbc1d11c837c5baa987f904
jvenske <20231508@mywhitecliffe.com>
2024-06-18 19:16:29 +1200

    how to firebase?

--- book-club/src/components/CurrentRead.vue
+++ book-club/src/components/CurrentRead.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="currentRead">
     <h2>Current Read</h2>
+    <p>{{ currentRead }}</p>
   </div>
 </template>
 
 <script>
-import { ref } from 'vue'
-import { projectFirestore } from '@/firebase/config';
+import getCurrentRead from '@/composables/getCurrentRead';
 
 export default {
    setup() {
+      const currentRead = getCurrentRead();
 
+      currentRead
+      console.log(currentRead)
    }
 }
 </script>
--- book-club/src/composables/getCurrentRead.js
+++ book-club/src/composables/getCurrentRead.js
@@ -1,23 +1,22 @@
 import { ref } from 'vue'
 import { projectFirestore } from '../firebase/config'
 
-const getCurrentRead = () => {
+const getCurrentRead = async () => {
+    console.log('getCurrentRead')
     const currentRead = ref(null)
     const error = ref(null)
 
-    const load = async () => {
-        try {
-            const res = await projectFirestore.collection('currentRead').get()
-            currentRead.value = res.docs.map(doc => {
-                return { ...doc.data(), id: doc.id }
-            })
-        } catch (err) {
-            error.value = err.message
-            console.log(error.value)
-        }
+    try {
+        const res = await projectFirestore.collection('books').get()
+        currentRead.value = res.docs.map(doc => {
+
+            return { ...doc.data(), id: doc.id }
+        })
+    } catch (err) {
+        error.value = err.message
+        console.log(error.value)
     }
 
-    return { currentRead, error, load }
 }
 
 export default getCurrentRead
